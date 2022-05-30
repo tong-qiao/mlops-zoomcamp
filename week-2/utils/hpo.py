48cce532fbdde9913bd5667675a06c872cb6e40a
Tong Qiao <tong.qiao@healx.io>
2022-05-30 18:17:50 +0100

    Finish HPO experiment

--- week-2/utils/hpo.py
+++ week-2/utils/hpo.py
@@ -24,13 +24,16 @@ def run(data_path, num_trials):
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
     def objective(params):
+        with mlflow.start_run():
+            mlflow.set_tag("model", "random-forest-regressor")
+            mlflow.log_params(params)
+            rf = RandomForestRegressor(**params)
+            rf.fit(X_train, y_train)
+            y_pred = rf.predict(X_valid)
+            rmse = mean_squared_error(y_valid, y_pred, squared=False)
+            mlflow.log_metric("rmse", rmse)
 
-        rf = RandomForestRegressor(**params)
-        rf.fit(X_train, y_train)
-        y_pred = rf.predict(X_valid)
-        rmse = mean_squared_error(y_valid, y_pred, squared=False)
-
-        return {'loss': rmse, 'status': STATUS_OK}
+            return {'loss': rmse, 'status': STATUS_OK}
 
     search_space = {
         'max_depth': scope.int(hp.quniform('max_depth', 1, 20, 1)),
