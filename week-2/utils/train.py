df96d2519e22c2731fc6048aa5db01cfbb93dc93
Tong Qiao <tong.qiao@healx.io>
2022-05-30 17:28:22 +0100

    Add MLflow autolog for model training

--- week-2/utils/train.py
+++ week-2/utils/train.py
@@ -1,9 +1,13 @@
 import argparse
 import os
 import pickle
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("homework-week-2")
 
 
 def load_pickle(filename: str):
@@ -13,14 +17,17 @@ def load_pickle(filename: str):
 
 def run(data_path):
 
+    mlflow.sklearn.autolog()
+
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
